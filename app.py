3d825c279660ad90f2e486dbdb30c2bba0edeb68
Abhay Pratap Singh <108487260+Mahadev2004@users.noreply.github.com>
2026-04-27 20:06:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -431,4 +431,5 @@ def get_dataset():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
